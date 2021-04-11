0acc4307be1a5d02cdc75c3aaccb23a15866e1dd
apl <antoineplddev@gmail.com>
2021-04-11 12:58:25 +0200

    add onoff

--- routes/socketRouting.js
+++ routes/socketRouting.js
@@ -1,10 +1,13 @@
-
+const Gpio = require('onoff').Gpio;
 
 function socketRouting(socket) {
 
     socket.on("servo-control-topic", data => {
         console.log({ data });
         socket.emit("servo-control-topic", data);
+
+        const servo = new Gpio(12, 'out');
+        servo.write(20);
     });
 
 }
